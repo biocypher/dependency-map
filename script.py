00323890d6a4c43d521669b126569072c738ad72
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2022-07-26 12:44:59 +0200

    performance profiling

--- script.py
+++ script.py
@@ -1,5 +1,31 @@
+import cProfile
+import io
+import pstats
+
 from adapter import BioCypherAdapter
 
-adapter = BioCypherAdapter(db_name="import")
+PROFILE = False
 
-adapter.write_to_csv_for_admin_import()
+
+def main():
+    if PROFILE:
+        profile = cProfile.Profile()
+        profile.enable()
+
+    adapter = BioCypherAdapter(db_name="import")
+
+    adapter.write_to_csv_for_admin_import()
+
+    if PROFILE:
+        profile.disable()
+
+        s = io.StringIO()
+        sortby = pstats.SortKey.CUMULATIVE
+        ps = pstats.Stats(profile, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        # print(s.getvalue())
+        ps.dump_stats("adapter.prof")
+
+
+if __name__ == "__main__":
+    main()
